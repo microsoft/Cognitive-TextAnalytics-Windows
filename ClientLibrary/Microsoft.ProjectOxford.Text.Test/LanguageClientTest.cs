fdb0fec11040cc8a1127f59dfe06e3e43f2a4cd5
adam grocholski <adgroc@outlook.com>
2016-10-28 11:04:32 -0500

    test stubs for language client

---
+++ ClientLibrary/Microsoft.ProjectOxford.Text.Test/LanguageClientTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Text.Core;
+using Microsoft.ProjectOxford.Text.Language;
+
+namespace Microsoft.ProjectOxford.Text.Test
+{
+    [TestClass]
+    public class LanguageClientTest
+    {
+        [TestMethod]
+        [TestCategory("Language Detection")]
+        public void GetLanguagesTest_OneLangage()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        [TestCategory("Language Detection")]
+        public void GetLanguagesTest_TwoLangages()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
