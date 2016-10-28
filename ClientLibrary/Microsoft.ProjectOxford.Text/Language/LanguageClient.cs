c5c5f4a4196f2b91acfa946c53ba14cd5efda862
adam grocholski <adgroc@outlook.com>
2016-10-28 09:19:14 -0500

    stubs for language detection

---
+++ ClientLibrary/Microsoft.ProjectOxford.Text/Language/LanguageClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Text.Core;
+using Newtonsoft.Json;
+
+namespace Microsoft.ProjectOxford.Text.Language
+{
+    public class LanguageClient : TextClient
+    {
+        public LanguageClient()
+        {
+            this.Url = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/languages";
+        }
+
+        public LanguageResponse GetLanguages(LanguageRequest request)
+        {
+            return GetLanguagesAsync(request).Result;
+        }
+
+        public async Task<LanguageResponse> GetLanguagesAsync(LanguageRequest request)
+        {
+            var url = this.Url;
+
+            if (request.NumberOfLanguagesToDetect > 1)
+                url = string.Format("{0}?numberOfLanguagesToDetect={1}", url, request.NumberOfLanguagesToDetect);
+
+            var json = JsonConvert.SerializeObject(request);
+            var responseJson = await this.SendPostAsync(url, json);
+            var response = JsonConvert.DeserializeObject<LanguageResponse>(responseJson);
+            return response;
+        }
+    }
+}
---
+++ ClientLibrary/Microsoft.ProjectOxford.Text/Language/LanguageRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ProjectOxford.Text.Language
+{
+    public class LanguageRequest
+    {
+        public LanguageRequest()
+        {
+            this.NumberOfLanguagesToDetect = 1;
+        }
+
+        public int NumberOfLanguagesToDetect{ get; set; }
+    }
+}
