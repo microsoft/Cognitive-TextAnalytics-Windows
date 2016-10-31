d1305109682cf92581fc92434ee0e1813b55d016
adam grocholski <adgroc@outlook.com>
2016-10-31 14:54:30 -0500

    document collection unit tests

---
+++ ClientLibrary/Microsoft.ProjectOxford.Text.Test/TextRequestTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.ProjectOxford.Text.Core;
+
+namespace Microsoft.ProjectOxford.Text.Test
+{
+    [TestClass]
+    public class TextRequestTest
+    {
+        [TestMethod]
+        [TestCategory("Request Validation")]
+        [ExpectedException(typeof(DocumentCollectionMinDocumentException))]
+        public void ValidateTest_MinDocuments()
+        {
+            var request = new MockRequest();
+            request.Validate();
+        }
+
+        [TestMethod]
+        [TestCategory("Request Validation")]
+        [ExpectedException(typeof(DocumentCollectionMaxDocumentException))]
+        public void ValidateTest_MaxDocuments()
+        {
+            var request = new MockRequest();
+
+            for(int i = 1; i<=1001; i++)
+            {
+                request.Documents.Add(new Document() { Id = i.ToString(), Text = "test test test" });
+            }
+
+            request.Validate();
+        }
+    }
+
+    public class MockRequest : TextRequest { }
+}
