df010386d114fa3b80fa651a2694800ec62bfaaf
adam grocholski <adgroc@outlook.com>
2016-10-31 10:27:58 -0500

    more packaging updates

--- ClientLibrary/NuGet/package.bat
+++ ClientLibrary/NuGet/package.bat
@@ -1,6 +1,6 @@
 set packageSourceDir=PackageSource
 if not exist %packageSourceDir% (mkdir %packageSourceDir%)
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" "..\Microsoft.ProjectOxford.Text\Microsoft.ProjectOxford.Text.csproj" /p:Configuration=Release
-nuget pack "..\Microsoft.ProjectOxford.Text\Microsoft.ProjectOxford.Text.csproj" -OutputDirectory %packageSourceDir%
+nuget pack "..\Microsoft.ProjectOxford.Text\Microsoft.ProjectOxford.Text.csproj" -Prop Configuration=Release -OutputDirectory %packageSourceDir%
 explorer %packageSourceDir%
 timeout /t -1
