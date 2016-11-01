4eee63c375de918f69d6b4f71d342888a7982fdb
adam grocholski <adgroc@outlook.com>
2016-11-01 15:23:48 -0500

    more basic ui work

--- Sample-WPF/MainWindow.xaml.cs
+++ Sample-WPF/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Microsoft.ProjectOxford.Text.Controls;
+using SampleUserControlLibrary;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +25,100 @@ namespace TextAPI_WPF_Samples
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructors
+
         public MainWindow()
         {
             InitializeComponent();
+            ServicePointManager.DefaultConnectionLimit = 1000;
+
+            this.ViewModel = new MainViewModel()
+            {
+                LanguageIdentificationDescription = "Identify the language text is written in."
+            };
+
+            this.DataContext = this.ViewModel;
+
+            this._scenariosControl.SampleScenarioList = new Scenario[]
+            {
+                new Scenario()
+                {
+                    PageClass = typeof(LanguageIdentificationPage),
+                    Title = "Language Identification"
+                }
+            };
         }
+
+        #endregion Constructors
+
+        #region Properties
+
+        /// <summary>
+        /// Gets view model instance for MainWindow
+        /// </summary>
+        public MainViewModel ViewModel
+        {
+            get; private set;
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        /// <summary>
+        /// Log message in main window log pane
+        /// </summary>
+        /// <param name="format">format string</param>
+        /// <param name="args">format arguments</param>
+        public static void Log(string format, params object[] args)
+        {
+            ((MainWindow)Application.Current.MainWindow)._scenariosControl.Log(string.Format(format, args));
+        }
+
+        #endregion Methods
+
+        #region Nested Types
+
+        public class MainViewModel : INotifyPropertyChanged
+        {
+            #region Events
+
+            /// <summary>
+            /// Implements INotifyPropertyChanged interface
+            /// </summary>
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            #endregion Events
+
+            #region Properties
+
+            public string LanguageIdentificationDescription
+            {
+                get;
+                set;
+            }
+
+            #endregion Properties
+
+            #region Methods
+
+            /// <summary>
+            /// Helper function for INotifyPropertyChanged interface 
+            /// </summary>
+            /// <typeparam name="T">Property type</typeparam>
+            /// <param name="caller">Property name</param>
+            private void OnPropertyChanged<T>([CallerMemberName]string caller = null)
+            {
+                var handler = PropertyChanged;
+                if (handler != null)
+                {
+                    handler(this, new PropertyChangedEventArgs(caller));
+                }
+            }
+
+            #endregion Methods
+        }
+
+        #endregion Nested Types
     }
 }
