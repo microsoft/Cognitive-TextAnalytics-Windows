e9c5a1ef0f473aad79646ce19097f558d07f27c7
adam grocholski <adgroc@outlook.com>
2016-11-23 13:46:37 -0600

    language client sync example for #16

--- Samples/Console/LanguageClientSample/LanguageClientSync/Program.cs
+++ Samples/Console/LanguageClientSample/LanguageClientSync/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.ProjectOxford.Text.Core;
+using Microsoft.ProjectOxford.Text.Language;
+using System;
 
 namespace LanguageClientSync
 {
@@ -10,6 +8,42 @@ namespace LanguageClientSync
     {
         static void Main(string[] args)
         {
+            var apiKey = "YOUR-TEXT-ANALYTICS-API-SUBSCRIPTION-KEY";
+
+            var document = new Document()
+            {
+                Id = "YOUR-UNIQUE-ID",
+                Text = "YOUR-TEXT"
+            };
+
+            var client = new LanguageClient(apiKey);
+
+            var request = new LanguageRequest();
+            request.Documents.Add(document);
+
+            try
+            {
+                var response = client.GetLanguages(request);
+
+                foreach (var doc in response.Documents)
+                {
+                    Console.WriteLine("Document Id: {0}", doc.Id);
+
+                    foreach (var lang in doc.DetectedLanguages)
+                    {
+                        Console.WriteLine("--Language: {0}({1})", lang.Name, lang.Iso639Name);
+                        Console.WriteLine("--Confidence: {0}%", (lang.Score * 100));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
         }
     }
 }
