e36697ff8dab4ba3c2a89d6f8d0da3b39fab18ef
adam grocholski <adgroc@outlook.com>
2016-12-21 09:44:45 -0600

    keyphraserequest cls

---
+++ ClientLibrary/Microsoft.ProjectOxford.Text/KeyPhrase/KeyPhraseRequest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.ProjectOxford.Text.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ProjectOxford.Text.KeyPhrase
+{
+    /// <summary>
+    /// Request for interacting with the Text Analytics key phrase detection API.
+    /// </summary>
+    /// <seealso cref="Microsoft.ProjectOxford.Text.Core.TextRequest" />
+    public class KeyPhraseRequest : TextRequest
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KeyPhraseRequest"/> class.
+        /// </summary>
+        public KeyPhraseRequest()
+        {
+            this.Documents = new List<IDocument>();
+        }
+
+        #endregion  Constructors
+    }
+}
