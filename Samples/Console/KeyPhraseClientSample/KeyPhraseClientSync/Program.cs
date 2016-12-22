021f5aa082ebfaf6cbe48327bdcd053ee29ad2f6
adam grocholski <adgroc@outlook.com>
2016-12-22 10:10:32 -0600

    sync console sample for #6

--- Samples/Console/KeyPhraseClientSample/KeyPhraseClientSync/Program.cs
+++ Samples/Console/KeyPhraseClientSample/KeyPhraseClientSync/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.ProjectOxford.Text.KeyPhrase;
+using System;
 
 namespace KeyPhraseClientSync
 {
@@ -10,6 +7,35 @@ namespace KeyPhraseClientSync
     {
         static void Main(string[] args)
         {
+            var apiKey = "YOUR-TEXT-ANALYTICS-API-SUBSCRIPTION-KEY";
+
+            var document = new KeyPhraseDocument()
+            {
+                Id = "YOUR-UNIQUE-ID",
+                Text = "YOUR-TEXT",
+                Language = "en"
+            };
+
+            var request = new KeyPhraseRequest();
+            request.Documents.Add(document);
+
+            var client = new KeyPhraseClient(apiKey);
+
+            var response = client.GetKeyPhrases(request);
+
+            foreach (var doc in response.Documents)
+            {
+                Console.WriteLine("Document Id: {0}", doc.Id);
+
+                foreach (var keyPhrase in doc.KeyPhrases)
+                {
+                    Console.WriteLine("   Key Phrase: {0}", keyPhrase);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
         }
     }
 }
