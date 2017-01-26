e13cd3b1661860edff80276adbd356a0e7c3f011
adam grocholski <adgroc@outlook.com>
2017-01-26 14:51:48 -0600

    TopicAssignment class for #7

---
+++ ClientLibrary/Microsoft.ProjectOxford.Text/Topic/TopicAssignment.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ProjectOxford.Text.Topic
+{
+    /// <summary>
+    /// Topic assignment.
+    /// </summary>
+    public class TopicAssignment
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the document identifier.
+        /// </summary>
+        /// <value>
+        /// The document identifier.
+        /// </value>
+        [JsonProperty("documentId")]
+        public string DocumentId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the topic identifier.
+        /// </summary>
+        /// <value>
+        /// The topic identifier.
+        /// </value>
+        [JsonProperty("topicId")]
+        public Guid TopicId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the distance.
+        /// </summary>
+        /// <value>
+        /// The distance.
+        /// </value>
+        [JsonProperty("distance")]
+        public float Distance { get; set; }
+
+        #endregion Properties
+    }
+}
