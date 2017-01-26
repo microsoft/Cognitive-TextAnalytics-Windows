93109561028777c3c72259012360228b21c77743
adam grocholski <adgroc@outlook.com>
2017-01-26 14:54:46 -0600

    TopicResult class for #7

---
+++ ClientLibrary/Microsoft.ProjectOxford.Text/Topic/TopicResult.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ProjectOxford.Text.Topic
+{
+    /// <summary>
+    /// Topic result object.
+    /// </summary>
+    public class TopicResult
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the score.
+        /// </summary>
+        /// <value>
+        /// The score.
+        /// </value>
+        [JsonProperty("score")]
+        public float Score { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key phrase.
+        /// </summary>
+        /// <value>
+        /// The key phrase.
+        /// </value>
+        [JsonProperty("keyPhrase")]
+        public string KeyPhrase { get; set; }
+
+        #endregion Properties
+    }
+}
