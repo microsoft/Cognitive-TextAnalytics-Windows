1b98caa7a0bb1c6a7ef4446c429318073f16bb10
adam grocholski <adgroc@outlook.com>
2017-01-26 17:07:33 -0600

    unit test min number of docs in collection

---
+++ ClientLibrary/Microsoft.ProjectOxford.Text.Test/TopicClientTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using Microsoft.ProjectOxford.Text.Core;
+using Microsoft.ProjectOxford.Text.Topic;
+
+namespace Microsoft.ProjectOxford.Text.Test
+{
+    /// <summary>
+    /// Unit tests for the TopicClient class.
+    /// </summary>
+    [TestClass]
+    public class TopicClientTest
+    {
+        #region Fields
+
+        private string apiKey = "";
+
+        #endregion Fields
+
+        #region Test Initialization
+
+        /// <summary>
+        /// Intializes this instance.
+        /// </summary>
+        [TestInitialize]
+        public void Intialize()
+        {
+            apiKey = ConfigurationManager.AppSettings["apiKey"];
+        }
+
+        #endregion Test Initialization
+
+        #region Test Methods
+
+        /// <summary>
+        /// Unit test of the Validate method for the minimum number of documents in a collection.
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Topic Detection")]
+        [ExpectedException(typeof(DocumentCollectionMinDocumentException))]
+        public void ValidateTest_MinDocumentCollectionCount()
+        {
+            var request = new TopicRequest();
+            request.Validate();
+        }
+
+        #endregion Test Methods
+    }
+}
