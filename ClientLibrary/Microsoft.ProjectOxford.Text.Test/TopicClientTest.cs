c60a418460280e50101fcb9eb2072395e717df9a
adam grocholski <adgroc@outlook.com>
2017-01-27 10:53:52 -0600

    unit tests for #7

--- ClientLibrary/Microsoft.ProjectOxford.Text.Test/TopicClientTest.cs
+++ ClientLibrary/Microsoft.ProjectOxford.Text.Test/TopicClientTest.cs
@@ -45,6 +45,102 @@ namespace Microsoft.ProjectOxford.Text.Test
             request.Validate();
         }
 
+        /// <summary>
+        /// Unit test of the Validate method for duplicate document identifiers.
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Request Validation")]
+        [ExpectedException(typeof(DocumentCollectionDuplicateIdException))]
+        public void ValidateTest_DuplicateDocumentId()
+        {
+            var request = new TopicRequest();
+
+            for (int i = 1; i <= 101; i++)
+            {
+                request.Documents.Add(new Document() { Id = "01", Text = "doc1" });
+            }
+
+            request.Validate();
+        }
+
+        /// <summary>
+        /// Unit test of the Validate method for document identifier.
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Request Validation")]
+        [ExpectedException(typeof(DocumentIdRequiredException))]
+        public void ValidateTest_DocumentIdRequired()
+        {
+            var request = new TopicRequest();
+
+            for (int i = 1; i <= 101; i++)
+            {
+                request.Documents.Add(new Document() { Text = "doc1" });
+            }
+
+            request.Validate();
+        }
+
+        /// <summary>
+        /// Unit test of the Validate method for the minimum document size.
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Request Validation")]
+        [ExpectedException(typeof(DocumentMinSizeException))]
+        public void ValidateTest_MinDocumentSize()
+        {
+            var request = new TopicRequest();
+
+            for (int i = 1; i <= 101; i++)
+            {
+                request.Documents.Add(new Document() { Id = i.ToString() });
+            }
+
+            request.Validate();
+        }
+
+        /// <summary>
+        /// Unit test of the Validate method for the maximum document size.
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Request Validation")]
+        [ExpectedException(typeof(DocumentMaxSizeException))]
+        public void ValidateTest_MaxDocumentSize()
+        {
+            int count = 10241 * 3;
+            var text = new string('*', count);
+
+            var request = new TopicRequest();
+
+            for (int i = 1; i <= 101; i++)
+            {
+                request.Documents.Add(new Document() { Id = i.ToString(), Text = text });
+            }
+
+            request.Validate();
+        }
+
+        /// <summary>
+        /// Unit test of the Validate method for the maximum size of a document collections.
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Request Validation")]
+        [ExpectedException(typeof(DocumentCollectionMaxSizeException))]
+        public void ValidateTest_MaxDocumentCollectionSize()
+        {
+            var count = 1024 * 31;
+            var text = new string('*', 1024);
+
+            var request = new TopicRequest();
+
+            for (int i = 1; i <= count; i++)
+            {
+                request.Documents.Add(new Document() { Id = i.ToString(), Text = text });
+            }
+
+            request.Validate();
+        }
+
         #endregion Test Methods
     }
 }
