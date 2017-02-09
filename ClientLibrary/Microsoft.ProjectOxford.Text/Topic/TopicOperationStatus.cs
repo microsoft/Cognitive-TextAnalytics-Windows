7e2fe3851ed9162140f92720d7a520db60ae9156
adam grocholski <adgroc@outlook.com>
2017-02-09 08:58:57 -0600

    updated package for console sample and added op status enum

---
+++ ClientLibrary/Microsoft.ProjectOxford.Text/Topic/TopicOperationStatus.cs
@@ -0,0 +1,33 @@
+﻿namespace Microsoft.ProjectOxford.Text.Topic
+{
+    /// <summary>
+    /// Status of operation submitted for document detection.
+    /// </summary>
+    public enum TopicOperationStatus
+    {
+        /// <summary>
+        /// Opeation not started
+        /// </summary>
+        NotStarted,
+
+        /// <summary>
+        /// Operation is running
+        /// </summary>
+        Running,
+
+        /// <summary>
+        /// Operation failed
+        /// </summary>
+        Failed,
+
+        /// <summary>
+        /// Operation cancelled
+        /// </summary>
+        Cancelled,
+
+        /// <summary>
+        /// Operation succeeded
+        /// </summary>
+        Succeeded
+    }
+}
