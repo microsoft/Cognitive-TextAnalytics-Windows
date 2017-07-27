531a1ac24b09734adb3c502780308d39388a13cf
Adam Grocholski <adgroc@outlook.com>
2017-07-27 12:08:01 -0500

    deprecated project

--- ClientLibrary/NuGet/package.bat
+++
@@ -1,6 +0,0 @@
-set packageSourceDir=PackageSource
-if not exist %packageSourceDir% (mkdir %packageSourceDir%)
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" "..\Microsoft.ProjectOxford.Text\Microsoft.ProjectOxford.Text.csproj" /p:Configuration=Release
-nuget pack "..\Microsoft.ProjectOxford.Text\Microsoft.ProjectOxford.Text.csproj" -Prop Configuration=Release -OutputDirectory %packageSourceDir%
-explorer %packageSourceDir%
-timeout /t -1
